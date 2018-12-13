63e214f9baa521cf4a46310109b94395993f5a9b
Alejandro Ubalton <aleubalton@gmail.com>
2018-12-13 17:08:04 -0300

    Cambios de estilo generales y traducciones

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JhiLanguageService } from 'ng-jhipster';
+
+import { ProfileService } from '../profiles/profile.service';
+import { JhiLanguageHelper, Principal, LoginModalService, LoginService } from 'app/core';
+
+import { VERSION } from '../../app.constants';
 
 @Component({
     selector: 'jhi-footer',
-    templateUrl: './footer.component.html'
+    templateUrl: './footer.component.html',
+    styleUrls: ['footer.scss']
 })
-export class FooterComponent {}
+export class FooterComponent implements OnInit {
+    inProduction: boolean;
+    languages: any[];
+    swaggerEnabled: boolean;
+
+    constructor(
+        private languageService: JhiLanguageService,
+        private languageHelper: JhiLanguageHelper,
+        private profileService: ProfileService
+    ) {}
+
+    ngOnInit() {
+        this.languageHelper.getAll().then(languages => {
+            this.languages = languages;
+        });
+
+        this.profileService.getProfileInfo().then(profileInfo => {
+            this.inProduction = profileInfo.inProduction;
+            this.swaggerEnabled = profileInfo.swaggerEnabled;
+        });
+    }
+
+    changeLanguage(languageKey: string) {
+        this.languageService.changeLanguage(languageKey);
+    }
+}
